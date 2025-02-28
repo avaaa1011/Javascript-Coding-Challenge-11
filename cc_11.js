41b461a32e7c0e9f5c5224f210a1324f62c945d4
Ava Tran <ava.tran1011@gmail.com>
2025-02-27 22:12:56 -0500

    task 2: created borrower class

--- cc_11.js
+++ cc_11.js
@@ -24,3 +24,30 @@ console.log(book1.getDetails());
 book1.updateCopies(-1);
 console.log(book1.getDetails());
 // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+//task 2: creating Borrower class
+class Borrower //new class with multiple properties 
+{
+    constructor(name, borrowerId)
+    {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+    borrowBook(book) //adding a method that adds books to borrowedBooks
+    {
+        this.borrowedBooks.push(book); //adds books 
+    }
+    returnBook (book) //method that removes books from borrowedBooks 
+    {
+        this.borrowedBooks =  this.borrowedBooks.filter(b => b !== book); //removes books 
+    }
+}
+//test data:
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: ["The Great Gatsby"]
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+// Expected output: []
